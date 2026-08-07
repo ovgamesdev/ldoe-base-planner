a49e9a123ae5a489acc0eaf269edc4b36bd93cd9
ovgamesdev <vova40227@gmail.com>
2026-08-07 05:18:14 +0500

    add (robodog_docking_station and atv_garage)

---
+++ scripts/resixe_4x5.ps1
@@ -0,0 +1,26 @@
+# === НАСТРОЙКИ РАЗМЕРОВ (вводите свои значения сюда) ===
+$origWidth  = 1800   # Исходная ширина картинки
+$origHeight = 2200   # Исходная высота картинки
+
+$diff = 1
+
+$cropLeft   = 156 + $diff    # Отрезать слева
+$cropRight  = 155 + $diff    # Отрезать справа
+$cropTop    = 170 + $diff    # Отрезать сверху
+$cropBottom = 169 + $diff    # Отрезать снизу
+# =======================================================
+
+# Автоматический расчет геометрии для ImageMagick
+$targetWidth  = $origWidth - $cropLeft - $cropRight
+$targetHeight = $origHeight - $cropTop - $cropBottom
+$offsetX      = $cropLeft
+$offsetY      = $cropTop
+
+# Собираем строку параметров (получится что-то вроде "372x373+54+81")
+$cropGeometry = "${targetWidth}x${targetHeight}+$offsetX+$offsetY"
+$resizeGeometry = "${origWidth}x${origHeight}"
+
+# Запуск обработки файлов
+Get-ChildItem *.png | ForEach-Object {
+    magick $_.FullName -crop $cropGeometry +repage -resize $resizeGeometry "$($_.BaseName).webp"
+}
